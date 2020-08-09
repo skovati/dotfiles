7405228d82487ab434101fa33d138271cd651ff4
skovati <skovati@protonmail.com>
2020-08-09 15:48:06 -0500

    Add seperate script for just symlinking (gentoo)

---
+++ symlinks.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+ln -s $REPO/vim/.vimrc $HOME/.vimrc
+ln -s $REPO/zsh/.zshrc $HOME/.zshrc
+ln -s $REPO/zsh/.zprofile $HOME/.zprofile
+ln -s $REPO/polybar/config $HOME/.config/polybar/config
+ln -s $REPO/polybar/colors $HOME/.config/polybar/colors
+ln -s $REPO/polybar/modules $HOME/.config/polybar/modules
+ln -s $REPO/polybar/launch.sh $HOME/.config/polybar/launch.sh
+ln -s $REPO/sxhkd/sxhkdrc $HOME/.config/sxhkd/sxhkdrc
+ln -s $REPO/bspwm/auto $HOME/.config/bspwm/auto
+ln -s $REPO/bspwm/bspwmrc $HOME/.config/bspwm/bspwmrc
+ln -s $REPO/gpg/gpg-agent.conf $HOME/.gnupg/gpg-agent.conf
+ln -s $REPO/picom/picom.conf $HOME/.picom.conf
+ln -s $REPO/redshift/redshift.conf $HOME/.config/redshift/redshift.conf

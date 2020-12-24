96b97c85e3cac0d4dfee63d3b5e49aac3494e26f
skovati <skovati@protonmail.com>
2020-12-24 15:08:45 -0600

    add lf and alacritty to arch_install script

--- arch_install.sh
+++ arch_install.sh
@@ -16,7 +16,6 @@ mkdir -p $HOME/.config/polybar
 mkdir $HOME/.config/bspwm
 mkdir $HOME/.config/sxhkd
 mkdir $HOME/.gnupg
-mkdir $HOME/.config/redshift
 
 ln -s $REPO/vim/.vimrc $HOME/.vimrc
 ln -s $REPO/zsh/.zshrc $HOME/.zshrc
@@ -29,12 +28,13 @@ ln -s $REPO/polybar/launch.sh $HOME/.config/polybar/launch.sh
 ln -s $REPO/sxhkd/sxhkdrc $HOME/.config/sxhkd/sxhkdrc
 ln -s $REPO/bspwm/auto $HOME/.config/bspwm/auto
 ln -s $REPO/bspwm/bspwmrc $HOME/.config/bspwm/bspwmrc
+ln -s $REPO/alacritty/alacritty.yml $HOME/.config/alacritty/alacritty.yml
+ln -s $REPO/alacritty/colors.yml $HOME/.config/alacritty/colors.yml
+ln -s $REPO/lf/lfrc $HOME/.config/lf/lfrc
 ln -s $REPO/gpg/gpg-agent.conf $HOME/.gnupg/gpg-agent.conf
 
 cd $HOME/code/git
 git clone https://github.com/skovati/scripts
-
-mkdir $HOME/.zsh
 
 mkdir $HOME/.fonts
 git clone https://github.com/sunaku/tamzen-font .fonts/tamzen-font

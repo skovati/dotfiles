022633efb9c11e6ea52dcc806ba69a6e94cab9f8
skovati <skovati@protonmail.com>
2021-05-17 20:17:55 -0500

    fix permissions for mkdir

--- docker_install.sh
+++ docker_install.sh
@@ -5,10 +5,13 @@
 ZSH_DIR=/usr/share/zsh/plugins
 REPO=/home/skovati/dev/git/dotfiles
 
+# mkdirs
+sudo mkdir $ZSH_DIR
+mkdir /home/skovati/.config/nvim
+
 # install zsh plugins
-mkdir $ZSH_DIR
-git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_DIR
-git clone https://github.com/zsh-users/zsh-syntax-highlighting $ZSH_DIR
+sudo git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_DIR
+sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting $ZSH_DIR
 
 # symlinks for tty dev
 ln -s $REPO/vim/.vimrc          $HOME/.vimrc

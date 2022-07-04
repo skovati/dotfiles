2654f511d249f3e8a79e9000deb47aba249b78b0
skovati <skovati@protonmail.com>
2022-07-03 21:16:43 -0700

    clean up nvim config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,64 +1,60 @@
-pcall(require("impatient"))
 ----------------------------------------
 -- bootstrap packer
 ----------------------------------------
 local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-local bootstrap = false
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-    bootstrap = true
+local bootstrap = vim.fn.empty(vim.fn.glob(install_path)) > 0
+if bootstrap then
+    vim.opt.guicursor = ""      -- don't change cursor on first boot
     vim.fn.system({
         "git", "clone",
         "https://github.com/wbthomason/packer.nvim",
         install_path,
     })
-    vim.cmd("packadd packer.nvim")
 end
 
 ----------------------------------------
 -- plugin declaration
 ----------------------------------------
-require("packer").startup(function(use)
-    use("wbthomason/packer.nvim")
-    use("tpope/vim-surround")
-    use({
-        "nvim-telescope/telescope.nvim",
-        requires = "nvim-lua/plenary.nvim"
-    })
-    use({
-        "numToStr/Comment.nvim",
-    })
-    use("neovim/nvim-lspconfig")
-    use("nvim-treesitter/nvim-treesitter")
-    use({
-        "hrsh7th/nvim-cmp",
-        requires = {
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-nvim-lsp"
-    }})
-    use({
-        "L3MON4D3/LuaSnip",
-        requires = "saadparwaiz1/cmp_luasnip"
-    })
-    use("nvim-lualine/lualine.nvim")
-    use("lewis6991/gitsigns.nvim")
-    use("lukas-reineke/indent-blankline.nvim")
-    use("akinsho/toggleterm.nvim")
-    use({
-        "TimUntersberger/neogit",
-        opt = true,
-        cmd = "Neogit"
-    })
-    use("lewis6991/impatient.nvim")
-    use("skovati/cmp-zk")
-    use("sainnhe/gruvbox-material")
+require("packer").startup({function(use)
+        use("wbthomason/packer.nvim")
+        use("tpope/vim-surround")
+        use({
+            "nvim-telescope/telescope.nvim",
+            requires = "nvim-lua/plenary.nvim"
+        })
+        use("numToStr/Comment.nvim")
+        use("neovim/nvim-lspconfig")
+        use("nvim-treesitter/nvim-treesitter")
+        use({
+            "hrsh7th/nvim-cmp",
+            requires = {
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+        }})
+        use({
+            "L3MON4D3/LuaSnip",
+            requires = "saadparwaiz1/cmp_luasnip"
+        })
+        use("nvim-lualine/lualine.nvim")
+        use("lewis6991/gitsigns.nvim")
+        use("lukas-reineke/indent-blankline.nvim")
+        use("akinsho/toggleterm.nvim")
+        use({
+            "TimUntersberger/neogit",
+            opt = true, cmd = "Neogit"
+        })
+        use("lewis6991/impatient.nvim")
+        use("skovati/cmp-zk")
+        use("sainnhe/gruvbox-material")
+        if bootstrap then
+            require("packer").sync()
+        end
+    end,
     config = {
-        compile_path = vim.fn.stdpath("config") .. "/lua/packer_compiled.lua"
+        compile_path = vim.fn.stdpath("config") .. "/lua/packer_compiled.lua",
     }
-    if bootstrap then
-        require("packer").sync()
-    end
-end)
+})
 
 if bootstrap then
     print("Installing plugins, restart nvim after packer sync completes")
@@ -70,45 +66,48 @@ vim.api.nvim_create_autocmd("BufWritePost", {
     pattern = "init.lua",
     group = vim.api.nvim_create_augroup("Packer", {}),
 })
+
+pcall(require("impatient"))
 ----------------------------------------
 -- sets
 ----------------------------------------
-vim.opt.relativenumber = true -- number line shows relative
-vim.opt.number = true -- and current line shows actual line nr
-vim.opt.smartindent = true -- indent according to lang
-vim.opt.tabstop = 4 -- 4 space tabs
-vim.opt.shiftwidth = 0 -- >> shifts by tabstop amount
-vim.opt.softtabstop = -1 -- backspace removes $tabstop spaces
-vim.opt.expandtab = true -- insert tabs as spaces
-vim.opt.clipboard = "unnamedplus" -- use system clipboard
-vim.opt.ignorecase = true -- ignore case in searches
-vim.opt.smartcase = true -- unless capital query
-vim.opt.guicursor = "" -- fixes alacritty changing cursor
-vim.opt.signcolumn = "number" -- combines the signcolumn and number columns
-vim.opt.timeoutlen = 600 -- decrease timeout length for keymaps
-vim.opt.showmode = false -- hide current mode
-vim.opt.updatetime = 250 -- decrease update time
-vim.opt.lazyredraw = true -- dont redraw screen when exec macros
-vim.opt.nrformats:append("alpha") -- let <Ctrl-a> do letters as well
+vim.opt.relativenumber = true          -- number line shows relative
+vim.opt.number = true                  -- and current line shows actual line nr
+vim.opt.smartindent = true             -- indent according to lang
+vim.opt.tabstop = 4                    -- 4 space tabs
+vim.opt.shiftwidth = 0                 -- >> shifts by tabstop amount
+vim.opt.softtabstop = -1               -- backspace removes $tabstop spaces
+vim.opt.expandtab = true               -- insert tabs as spaces
+vim.opt.clipboard = "unnamedplus"      -- use system clipboard
+vim.opt.ignorecase = true              -- ignore case in searches
+vim.opt.smartcase = true               -- ^ unless capital query
+vim.opt.guicursor = ""                 -- fixes alacritty changing cursor
+vim.opt.signcolumn = "number"          -- combines the signcolumn and number columns
+vim.opt.timeoutlen = 600               -- decrease timeout length for keymaps
+vim.opt.showmode = false               -- hide current mode, it's in statusline
+vim.opt.updatetime = 250               -- decrease update time
+vim.opt.lazyredraw = true              -- dont redraw screen when exec macros
+vim.opt.nrformats:append("alpha")      -- let <Ctrl-a> do letters as well
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 vim.opt.completeopt = { "menuone", "noselect", "preview" } -- set for cmp
-vim.opt.shortmess:append("c") -- dont show eg "1 out of 20 matches"
+vim.opt.shortmess:append("c")          -- dont show eg "1 out of 20 matches"
 vim.opt.conceallevel = 0
-vim.opt.mouse = "a" -- only used when pair programming dont judge
+vim.opt.mouse = "a"                    -- only used when pair programming dont judge
 vim.opt.cursorline = true
 vim.opt.termguicolors = true
-vim.opt.swapfile = false -- disable swapfiles
-vim.opt.backup = false -- and auto backps, to instead use
-vim.opt.undofile = true -- enable auto save of undos
-vim.opt.foldmethod = "expr" -- syntax folding: zc, zo, zr, zR
+vim.opt.swapfile = false               -- disable swapfiles
+vim.opt.backup = false                 -- and auto backps, to instead use
+vim.opt.undofile = true                -- enable auto save of undos
+vim.opt.foldmethod = "expr"            -- syntax folding: zc, zo, zr, zR
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.foldnestmax = 10
 vim.opt.foldenable = false
-vim.opt.spelllang = "en_us" -- spell check
+vim.opt.spelllang = "en_us"            -- spell check
 vim.opt.complete:append("kspell")
-vim.opt.listchars:append("trail:·") -- show trailing spaces
-vim.opt.list = true -- show things in listchars
+vim.opt.listchars:append("trail:·")    -- show trailing spaces
+vim.opt.list = true                    -- show things in listchars
+-- turn off relativenumber when in insert mode
 local toggle_rel_num = vim.api.nvim_create_augroup("ToggleRelNum", {})
 vim.api.nvim_create_autocmd("InsertEnter", {
     command = "set norelativenumber",
@@ -190,6 +189,10 @@ vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_tarPlugin = 1
 vim.g.loaded_zipPlugin = 1
 
+vim.g.indent_blankline_char = "¦"
+vim.g.indent_blankline_show_trailing_blankline_indent = false
+vim.g.indent_blankline_use_treesitter = true
+
 require("lualine").setup({
     options = {
         icons_enabled = false,
@@ -202,9 +205,7 @@ require("lualine").setup({
 
 require("gitsigns").setup({ signcolumn = false, numhl = true, })
 
-require("toggleterm").setup({
-    open_mapping = [[<c-\>]]
-})
+require("toggleterm").setup({ open_mapping = [[<c-\>]] })
 
 require("telescope").setup({
     defaults = {
@@ -220,14 +221,6 @@ require("telescope").setup({
     },
 })
 
--- indent
-vim.g.indent_blankline_char = "¦"
-vim.g.indent_blankline_show_trailing_blankline_indent = false
-vim.g.indent_blankline_use_treesitter = true
-
-----------------------------------------
--- treesitter
-----------------------------------------
 require("nvim-treesitter.configs").setup({
     ensure_installed = "all",
     ignore_install = { "phpdoc" },
@@ -258,39 +251,24 @@ local on_attach = function(_, bufnr)
 
     -- configure how lsp diagnostics are shown
     vim.diagnostic.config({
-        virtual_text = true,
-        signs = false,
-        underline = true,
-        update_in_insert = false,
+        virtual_text = true, signs = false,
+        underline = true, update_in_insert = false,
     })
 end
 
 -- setup specific LSPs
 local servers = {
     "pyright", "rust_analyzer", "gopls", "clangd",
-    "tsserver", "svls", "jdtls", "bashls", "hls", "sumneko_lua"
+    "tsserver", "svls", "jdtls", "bashls", "hls",
 }
 
 local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
 for _, lsp in ipairs(servers) do
-    local opts = {
+    lspconfig[lsp].setup({
         capabilities = capabilities,
         on_attach = on_attach,
         flags = { debounce_text_changes = 150 },
-    }
-    local lua_settings = {
-        settings = {
-            Lua = {
-                runtime = { version = "LuaJIT", },
-                diagnostics = { globals = { "vim" }, },
-                workspace = { library = vim.api.nvim_get_runtime_file("", true) },
-            },
-        }
-    }
-    if lsp == "sumneko_lua" then
-        opts = vim.tbl_extend("error", lua_settings, opts)
-    end
-    lspconfig[lsp].setup(opts)
+    })
 end
 
 -- auto-completion setup
@@ -312,12 +290,9 @@ cmp.setup({
         end, { "i" }),
     }),
     sources = {
-        { name = "nvim_lsp" },
-        { name = "luasnip" },
-        { name = "path" },
-        { name = "calc" },
-        { name = "buffer" },
-        { name = "zk" },
+        { name = "nvim_lsp" },  { name = "luasnip" },
+        { name = "path" },      { name = "calc" },
+        { name = "buffer" },    { name = "zk" },
     },
     snippet = {
         expand = function(args)
